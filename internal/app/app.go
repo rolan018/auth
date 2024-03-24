7b730df2f0f669ee92fd615835bae564e3ca6de0
rolan018 <dadashev.rr@phystech.edu>
2024-03-25 00:27:38 +0200

    add application

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,4 +1,22 @@
 package app
 
+import (
+	grpcapp "auth/internal/app/grpc"
+	"log/slog"
+	"time"
+)
+
 type App struct {
+	GRPCApp *grpcapp.App
+}
+
+// create New gRPC server app.
+func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
+	// TODO: инициализировать хранилище
+
+	// TODO: init auth service
+
+	grpcApp := grpcapp.New(log, grpcPort)
+
+	return &App{GRPCApp: grpcApp}
 }
