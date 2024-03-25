a70b3cbce3124ce3235b06026fca7e65ecb36766
rolan018 <dadashev.rr@phystech.edu>
2024-03-26 00:52:13 +0200

    implement login

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -3,8 +3,16 @@ package server
 import (
 	authv1 "auth/protos/gen/go"
 	"context"
+	"fmt"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	emptyStringValue = ""
+	emptyIntValue    = 0
 )
 
 type serverAPI struct {
@@ -17,6 +25,16 @@ func Register(gRPC *grpc.Server) {
 
 // Заглушки
 func (s *serverAPI) Login(ctx context.Context, req *authv1.LoginRequest) (*authv1.LoginResponse, error) {
+	if err := validateRequest(req.GetAppId(), emptyIntValue); err != nil {
+		return nil, err
+	}
+	if err := validateRequest(req.GetEmail(), emptyStringValue); err != nil {
+		return nil, err
+	}
+	if err := validateRequest(req.GetPassword(), emptyStringValue); err != nil {
+		return nil, err
+	}
+
 	return &authv1.LoginResponse{Token: req.GetEmail()}, nil
 }
 
@@ -26,4 +44,11 @@ func (s *serverAPI) Register(ctx context.Context, req *authv1.RegisterRequest) (
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *authv1.IsAdminRequest) (*authv1.IsAdminResponse, error) {
 	panic("implement me IsAdmin()")
+}
+
+func validateRequest(input interface{}, emptyValue interface{}) error {
+	if input == emptyValue {
+		return status.Error(codes.InvalidArgument, fmt.Sprintf("%s is required", input))
+	}
+	return nil
 }
